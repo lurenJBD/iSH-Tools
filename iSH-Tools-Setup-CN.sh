1bb7a5c66817015b2fe3facfd1970ba91935af99
路人去甲剩丙丁 <31967654+lurenJBD@users.noreply.github.com>
2024-02-10 00:02:33 +0800

    Improved feedback on script run results

--- iSH-Tools-Setup-CN.sh
+++ iSH-Tools-Setup-CN.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Moded by lurenJBD 2024.02.08
+# Moded by lurenJBD 2024.02.09
 # iSH-Tools by lurenJBD 2020.10.17
 
 ########### Variable ###########
@@ -9,37 +9,49 @@ HOST="baidu.com"
 NAMESERVER="223.5.5.5"
 
 RED='\033[0;31m'
+GREEN='\033[32m'
 YELLOW='\033[0;33m'
 CYAN='\033[0;36m'
 PLAIN='\033[0m'
-echo_INFO="echo -e "${CYAN}INFO${PLAIN}""
-echo_WARNING="echo -e "${YELLOW}WARNING${PLAIN}""
+
 echo_ERROR="echo -e "${RED}ERROR${PLAIN}""
+echo_OK="echo -e "${GREEN}OK${PLAIN}""
 
 ########### Function ###########
+# 提示打印函数
+printf_tips() {
+    local level=$1 context=$2 end_line=$3 
+    case $level in
+        info) printf "${CYAN}%-7s${PLAIN} %-8s $end_line" "INFO" "$context";;
+        warning) printf "${YELLOW}%-7s${PLAIN} %-8s $end_line" "WARNING" "$context";;
+        error) printf "${RED}%-7s${PLAIN} %-8s $end_line" "ERROR" "$context";;
+    esac
+}
+
 # 检查网络状况
 check_connection() {
     ping_host() {
         ping -4 -c 1 -w 1 -A $HOST &>/dev/null
     }
-    $echo_INFO 正在检查网络状况...
+    printf_tips info "正在检查网络状况..." 
     if ! ping_host; then
-        $echo_WARNING 网络连接异常，尝试更改DNS重新测试
+        $echo_ERROR && printf_tips warning "网络连接检测失败，尝试更改DNS再次测试..."
         cp /etc/resolv.conf /etc/resolv.conf.bak
         echo "nameserver ${NAMESERVER}" > /etc/resolv.conf
         if ! ping_host; then
             mv /etc/resolv.conf.bak /etc/resolv.conf
-            No_Network=1
+            $echo_ERROR && return 1
         fi
     fi
+    $echo_OK
 }
 
 # 检查所属地区，决定是否使用镜像站
 check_location() {
     location=$(wget -qO- https://cf-ns.com/cdn-cgi/trace | awk -F'=' '/^loc=/{print $2}')
     if [[ "$location" == "CN" ]]; then
-        $echo_INFO "根据当前网络环境，建议临时更换镜像源并使用GitHub镜像站"
-        read -p "[*] 是否要使用镜像源? [Y/N]" user_choice
+        printf_tips info "根据当前网络环境，建议临时更换镜像源并使用GitHub镜像站" "\n"
+        read -p "[*]  是否要使用镜像源? [Y/N]" user_choice && printf '\033[A' && printf '\r\033[K'
         case $user_choice in
         [yY])
             github_url="https://mirror.ghproxy.com/https://github.com"
@@ -48,7 +60,7 @@ check_location() {
             echo "http://mirrors.tuna.tsinghua.edu.cn/alpine/$alpine_version/community" >>/etc/apk/repositories
             ;;
         [nN]|*)
-            $echo_INFO 已选择不使用镜像源，如果遇到访问缓慢可以再次运行本工具修改
+            printf_tips warning "已选择不使用镜像源，如果访问缓慢可以再次运行本工具修改" "\n"
             ;;
         esac
     fi
@@ -69,7 +81,7 @@ run_timer() {
         sleep 1
         let execution_time+=1 
         if [ "$execution_time" -gt "$timeout" ]; then
-            $echo_WARNING "超过${timeout}s未完成安装，${timeout_tip}"
+            printf "\n${YELLOW}%-7s${PLAIN} %-8s" "WARNING" "超过${timeout}s未完成安装，${timeout_tip}"
             break
         fi
     done
@@ -79,14 +91,22 @@ run_timer() {
 set_apk_repo() {
     run_timer "apk add" 30 "可能是源下载太慢，建议使用镜像源" &
     apk add -q ${inite_repo}
-    exit_code=$?
-    if [ $exit_code = 0 ]; then
+    printf '\r\033[A' && printf_tips info "正在安装需要的软件包..."
+    if [ $? = 0 ]; then
         sed -i "s#::sysinit:/sbin/openrc sysinit#::sysinit:/sbin/openrc#g" /etc/inittab
-        echo inited_repo=\"$inite_repo\" >>/etc/iSH-Tools/tools_inited
+        echo inited_repo=\"$inite_repo\" >>/etc/iSH-Tools/tools_inited 
+        $echo_OK
     else
-        $echo_ERROR "安装软件包失败，请检查网络并重试" && exit 1
+        $echo_ERROR
+        printf_tips error "安装软件包失败，请检查网络并重试" "\n" && exit 1
     fi
 }
+
+# 脚本退出前执行
+shutdown() {
+    printf '\r\033[K'
+}
+trap shutdown EXIT
 
 # 初始化并安装最新版iSH-Tools
 run_main() {
@@ -97,11 +117,11 @@ run_main() {
         ish_ver=$(sed 's/.* iSH \([0-9]\.[0-9]\.[0-9]\) (\([0-9]\{1,\}\)) \(.*\)/\1 (\2)/' /proc/version)
     fi
     if ! [[ "$ish_type" =~ "iSH" ]]; then
-        $echo_ERROR 未知的iSH版本，脚本尚未支持 && exit 1
+        printf_tips error "未知的iSH版本，脚本尚未支持" "\n" && exit 1
     fi
     alpine_version=$(awk -F. '{if ($1 == 3) print "v3."$2}' /etc/alpine-release)
     if [[ -z "$alpine_version" ]]; then
-        $echo_ERROR 非alpine系统，脚本不支持运行 && exit 1
+        printf_tips error "非alpine系统，脚本不支持运行" "\n" && exit 1
     fi
     # 2.X版本的旧配置文件清理
     if [ -e /opt/iSH-VNC/VNC_installed ]; then
@@ -111,31 +131,39 @@ run_main() {
         rm -rf /opt/iSH-VNC
     fi
     # 进行网络连接检查
-    check_connection
-    if [ "$No_Network" = 1 ]; then
-        $echo_ERROR 无网络链接，无法安装/更新 iSH-Tools && exit 1
+    check_connection || { printf_tips error "无网络，无法安装/更新 iSH-Tools" "\n" && exit 1; }
+    check_location
+    printf_tips info "正在获取 iSH-Tools 最新版本号..."
+    lastest_version=$(wget -T10 -qO- ${github_url}/lurenJBD/iSH-Tools/raw/main/lastest_version 2>/dev/null | awk -F'=' '/^lastest_version=/{print $2}')
+    installed_tip="已经安装最新版本"
+    if [[ -z "$lastest_version" ]]; then
+        $echo_ERROR && printf_tips error "获取 iSH-Tools 最新版本号失败，请检查网络并重试" "\n" && exit 1
     else
-        check_location
-        lastest_version=$(wget -T10 -qO- ${github_url}/lurenJBD/iSH-Tools/raw/main/lastest_version 2>/dev/null | awk -F'=' '/^lastest_version=/{print $2}')
-        installed_tip="已经安装最新版本"
-        if [[ -z "$lastest_version" ]]; then
-            $echo_ERROR "获取 iSH-Tools 最新版本号失败，请检查网络并重试" && exit 1
-        fi
+        $echo_OK
     fi
     # 安装基础依赖包
     if [ ! -e /etc/iSH-Tools/tools_inited ];then
         mkdir -p /etc/iSH-Tools
-        $echo_INFO 正在安装需要的软件包...
+        printf_tips info "正在安装需要的软件包..." "\n"
         set_apk_repo
     else
         source /etc/iSH-Tools/tools_inited
         if [[ -n "$inited_version" ]]; then
-            $echo_INFO "检测到已安装 iSH-Tools $inited_version 版本"
+            printf_tips info "检测到已安装 iSH-Tools $inited_version 版本" "\n"
             if [ $(echo "$lastest_version > $inited_version" | bc) -eq 1 ]; then
-                $echo_INFO 检查到新版本，自动更新中...
+                printf_tips info "检查到新版本，自动更新中..."
+                rm -f /usr/local/bin/iSH-Tools && sed -i '/inited_version/d' /etc/iSH-Tools/tools_inited
                 installed_tip="已经更新为"
-                rm -f /etc/iSH-Tools/tools_inited /usr/local/bin/iSH-Tools
-                set_apk_repo
+                for word in $inite_repo; do
+                    if ! echo "$inited_repo" | grep -q "$word"; then
+                        need_inite_repo=1
+                        break
+                    fi
+                done
+                if [ "$need_inite_repo" = 1 ]; then
+                    rm -f /etc/iSH-Tools/tools_inited
+                    set_apk_repo
+                fi
             fi
         fi
     fi
@@ -147,10 +175,10 @@ run_main() {
             chmod +x /usr/local/bin/iSH-Tools
         else
             rm -f /usr/local/bin/iSH-Tools
-            $echo_ERROR "下载 iSH-Tools 失败，请检查网络并重试" && exit 1
+            printf_tips error "下载 iSH-Tools 失败，请检查网络并重试" "\n" && exit 1
         fi
     fi
-    $echo_INFO "${installed_tip} iSH-Tools ${lastest_version}，输入 iSH-Tools 开始使用"
+    printf_tips info "${installed_tip} iSH-Tools ${lastest_version}，输入 iSH-Tools 开始使用" "\n"
 }
 
 ########### Main ###########
